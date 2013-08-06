5e0fa3b3c5633f57a20a757298b6bf8d3f7c93d0
Steinn Steinsen <steinnsteinsen@gmail.com>
2013-08-06 12:33:11 +0000

    update custom.el

--- prelude-personal/custom.el
+++ prelude-personal/custom.el
@@ -3,8 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(background-color "#fdf6e3")
+ '(background-mode light)
+ '(cursor-color "#657b83")
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
  '(fci-rule-color "#383838")
- '(safe-local-variable-values (quote ((virtualenv-default-directory . "/home/steinn/work/birdcore") (python-run-file . "/home/steinn/work/birdcore/special/steinn/emacs_shell.py") (virtualenv-workon . "birdcore")))))
+ '(foreground-color "#657b83")
+ '(safe-local-variable-values (quote ((project-venv-name . "birdcore") (virtualenv-default-directory . "/home/steinn/work/birdcore") (python-run-file . "/home/steinn/work/birdcore/special/steinn/emacs_shell.py") (virtualenv-workon . "birdcore"))))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
