3f382bf76a0c2353af723a6f5685e0281045ae17
Steinn Steinsen <steinnsteinsen@gmail.com>
2014-02-05 15:34:42 -0800

    update my python.el

--- prelude-personal/python.el
+++ prelude-personal/python.el
@@ -5,7 +5,7 @@
 (require 'prelude-programming)
 (require 'cython-mode)
 
-(prelude-ensure-module-deps '(epc auto-complete jedi virtualenvwrapper))
+(prelude-ensure-module-deps '(epc auto-complete jedi virtualenvwrapper pytest))
 
 (setq HOME (file-name-as-directory (getenv "HOME")))
 (setq EHOME (file-name-as-directory (concat HOME ".emacs")))
@@ -26,32 +26,62 @@
  "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 
+;; Setup virtualenvwrapper
 (require 'virtualenvwrapper)
 (venv-initialize-interactive-shells)
 (venv-initialize-eshell)
-(setq venv-location (concat HOME ".virtualenvs"))
-
+(setq venv-location (expand-file-name "~/.virtualenvs/"))
 
 ;; Setup Jedi
 ;; Requires python packages: jedi and epc
-(setq jedi:setup-keys t)
 (autoload 'jedi:setup "jedi" nil t)
+(setq jedi:complete-on-dot t)
+(setq jedi:setup-keys t)
 
-;(add-to-list 'which-func-modes 'python-mode)
+;; Setup key bindings
+(setq jedi:key-goto-definition (kbd "M-."))
+(setq jedi:key-goto-definition-pop-marker (kbd "M-,"))
+(setq jedi:key-show-doc (kbd "M-?"))
+(setq jedi:key-related-names (kbd "C-c j r"))
+
+;; Do not add --virtual-env arg to
+(setq jedi:import-python-el-settings nil)
+
+(defun jedi-python-bin ()
+  "Calculate the string used to execute the inferior Python process."
+  (let ((process-environment (python-shell-calculate-process-environment))
+        (exec-path (python-shell-calculate-exec-path)))
+    (format "%s" (executable-find "python"))))
+
+(defun jedi-process-per-venv ()
+  (let ((cmds (list (jedi-python-bin) jedi:server-script ))
+        (args nil))
+    (when cmds (set (make-local-variable 'jedi:server-command) cmds))
+    (when args (set (make-local-variable 'jedi:server-args) args))))
+
 
 (require 'nose)
 
-(setq project-venv-name "base")
+(require 'pytest)
+(setq pytest-global-name "py.test")
+
+;; Set project-venv-name in .dir-locals to automatically
+;; switch to the venv name
+(setq project-venv-name nil)
+
 
 (defun prelude-python-mode-defaults ()
   "Python mode hook."
   (run-hooks 'prelude-prog-mode-hook)
+  (subword-mode +1)
   (jedi:setup)
+  (jedi-process-per-venv)
   (auto-complete-mode +1)
   (whitespace-mode +1)
   ; (electric-indent-mode -1)
   (hack-local-variables)
-  (venv-workon project-venv-name)
+  (if project-venv-name
+      (venv-workon project-venv-name))
   (setq nose-global-name (concat venv-current-dir "bin/nosetests"))
   (local-set-key "\C-c\C-ta" 'nosetests-all)
   (local-set-key "\C-c\C-tm" 'nosetests-module)
