fc3d6bc3007517e00789ce329ce6a01923f5f13c
Steinn Steinsen <steinnsteinsen@gmail.com>
2016-08-12 21:34:54 +0000

    custom

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (volatile-highlights flycheck scala-mode undo-tree rainbow-delimiters smartparens ace-window fill-column-indicator magit projectile helm zenburn-theme use-package-chords req-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
