25779c0737dfe2def28503b54f352a07dfee48db
Steinn Steinsen <steinnsteinsen@gmail.com>
2016-08-23 09:12:44 +0000

    config gdb-mi to not force window layout

--- init.el
+++ init.el
@@ -353,6 +353,18 @@
 
 (req-package thrift)
 
+(req-package 'gdb-mi
+  :config
+  ;; Force gdb-mi to not dedicate any windows
+  (defadvice gdb-display-buffer (after undedicate-gdb-display-buffer)
+    (set-window-dedicated-p ad-return-value nil))
+  (ad-activate 'gdb-display-buffer)
+
+  (defadvice gdb-set-window-buffer (after undedicate-gdb-set-window-buffer (name &optional ignore-dedi window))
+    (set-window-dedicated-p window nil))
+  (ad-activate 'gdb-set-window-buffer))
+
+
 (req-package-finish)
 
 (find-file "~/.emacs.d/init.el")
