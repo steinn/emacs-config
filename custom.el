19d60822869fb8b5b4b5ffc5f8b0c5b6c1fea781
Steinn Steinsen <steinnsteinsen@gmail.com>
2017-04-19 09:17:29 +0000

    custom.el

--- custom.el
+++ custom.el
@@ -4,9 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(flycheck-javascript-flow-args nil)
- '(package-selected-packages
+ '(safe-local-variable-values
    (quote
-    (zenburn-theme yasnippet which-key volatile-highlights virtualenvwrapper use-package-chords undo-tree thrift smartscan smartparens smart-mode-line scala-mode req-package rainbow-delimiters pyvenv projectile org omnisharp multiple-cursors magit js2-mode jedi helm-flycheck helm go-mode flycheck-flow fill-column-indicator eshell-z elisp-slime-nav ace-window))))
+    ((projectile-project-run-cmd . "mono ./DemoStats/bin/Release/DemoStats.exe csgo.dem")
+     (projectile-project-test-cmd . "nunit-console26 Tests/bin/Release/Tests.dll")
+     (projectile-project-compilation-cmd . "xbuild /verbosity:minimal /p:Configuration=Release")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
