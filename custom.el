9b4a3d9572c329432a18cc8fcb331adde244d57d
Steinn Steinsen <steinn@Steinns-MacBook-Pro.local>
2017-10-03 20:41:26 +0000

    custom

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (prettier-js zoom-frm zenburn-theme yaml-mode which-key volatile-highlights virtualenvwrapper utop use-package-chords undo-tree tuareg thrift terraform-mode tern-auto-complete smartscan smartparens smart-mode-line rust-mode req-package rainbow-delimiters pyvenv python-mode protobuf-mode php-mode omnisharp nginx-mode multiple-cursors markdown-mode magit lua-mode jedi jade helm-projectile helm-flycheck helm-ag go-mode flycheck-flow fill-column-indicator exec-path-from-shell eshell-z epresent ensime elisp-slime-nav ag ace-window))))
+    (zenburn-theme yaml-mode which-key volatile-highlights virtualenvwrapper utop use-package-chords undo-tree tuareg thrift terraform-mode smartscan smartparens smart-mode-line rust-mode req-package reason-mode rainbow-delimiters quelpa python-mode protobuf-mode omnisharp multiple-cursors merlin markdown-mode magit lua-mode js2-mode jedi helm-projectile helm-flycheck helm-ag go-mode flycheck-flow fill-column-indicator exec-path-from-shell eshell-z epresent ensime elisp-slime-nav ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
