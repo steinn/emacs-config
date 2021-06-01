4652ba124e570919412ca3cfee6addbd32431887
Steinn Steinsen <steinnsteinsen@gmail.com>
2021-06-01 09:00:37 +0000

    latest

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(counsel-projectile zenburn-theme yasnippet yaml-mode which-key web-mode volatile-highlights use-package-chords undo-tree tide smartscan smartparens smart-mode-line req-package rainbow-delimiters quelpa python-mode projectile prettier-js org-roam multiple-cursors modern-cpp-font-lock markdown-mode magit json-mode js2-mode graphviz-dot-mode glsl-mode flx-ido exec-path-from-shell eshell-z el-get editorconfig csharp-mode counsel company clang-format ag ace-window)))
+   '(prettier typescript rjsx-mode zenburn-theme yasnippet yaml-mode which-key web-mode volatile-highlights use-package-chords undo-tree tide smartscan smartparens smart-mode-line req-package rainbow-delimiters quelpa python-mode prettier-js org-roam multiple-cursors modern-cpp-font-lock markdown-mode magit json-mode js2-mode graphviz-dot-mode glsl-mode flx-ido exec-path-from-shell eshell-z el-get editorconfig csharp-mode counsel-projectile company clang-format ag ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -364,12 +364,6 @@
 ;; python
 (req-package python-mode)
 
-(req-package js2-mode
-  :require flycheck
-  :config
-  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
-  (setq js-indent-level 2))
 
 (req-package multiple-cursors
   :config
@@ -414,48 +408,50 @@
   :config
   (editorconfig-mode 1))
 
+;; (req-package js2-mode
+;;   :require flycheck
+;;   :config
+;;   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
+;;   (setq js-indent-level 2))
+(req-package rjsx-mode)
+
 ;; (req-package typescript)
 
-;; (req-package prettier-js
-;;   :require js2-mode
-;;   :config
-;;   (add-hook 'js2-mode-hook 'prettier-js-mode)
-;;   ;; (setq prettier-js-args '("--single-quote"
-;;   ;;                          "--trailing-comma" "es5"
-;;   ;;                          "--bracket-spacing" "false"
-;;   ;;                          "--parser" "flow"))
-;;   )
+(req-package prettier
+  :config
+  (add-hook 'js2-mode-hook 'prettier-js-mode))
 
-;; (req-package tide
-;;   :require prettier-js
-;;   :config
-;;   (defun setup-tide-mode ()
-;;     (interactive)
-;;     (tide-setup)
-;;     (flycheck-mode +1)
-;;     (setq flycheck-check-syntax-automatically '(save mode-enabled))
-;;     (eldoc-mode +1)
-;;     (tide-hl-identifier-mode +1)
-;;     (company-mode +1)
-;;     (prettier-js-mode +1))
+(req-package tide
+  :require prettier-js
+  :config
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    (company-mode +1)
+    (prettier-mode +1))
 
-;;   ;; aligns annotation to the right hand side
-;;   (setq company-tooltip-align-annotations t)
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
 
-;;   ;; don't use tide formatting, use prettier to format
-;;   ;; (add-hook 'before-save-hook 'tide-format-before-save)
+  ;; don't use tide formatting, use prettier to format
+  ;; (add-hook 'before-save-hook 'tide-format-before-save)
 
-;;   (add-hook 'typescript-mode-hook #'setup-tide-mode))
+  (add-hook 'typescript-mode-hook #'setup-tide-mode))
 
-;; (req-package web-mode
-;;   :config
-;;   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-;;   (add-hook 'web-mode-hook
-;;             (lambda ()
-;;               (when (string-equal "tsx" (file-name-extension buffer-file-name))
-;;                 (setup-tide-mode))))
-;;   ;; enable typescript-tslint checker
-;;   (flycheck-add-mode 'typescript-tslint 'web-mode))
+(req-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                (setup-tide-mode))))
+  ;; enable typescript-tslint checker
+  (flycheck-add-mode 'typescript-tslint 'web-mode))
 
 ;; (req-package lsp-mode
 ;;   :config
