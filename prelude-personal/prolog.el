2577bf51376afd7585eeb24d8f7e0c41d82a78c1
Steinn Steinsen <steinnsteinsen@gmail.com>
2022-10-03 21:00:19 +0000

    remove old prelude stuff

--- prelude-personal/prolog.el
+++
@@ -1,2 +0,0 @@
-(setq prolog-system 'swi)
-(setq auto-mode-alist (cons '("\\.pl$" . prolog-mode) auto-mode-alist))
--- prelude-personal/window-numbering.el
+++
@@ -1,3 +0,0 @@
-;; (prelude-ensure-module-deps '(window-numbering))
-;; (custom-set-faces '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold)))))
-;; (window-numbering-mode 1)
